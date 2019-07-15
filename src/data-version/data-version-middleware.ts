731155a8baffa10834642bde10872755e7dcd529
DoctorVoid <furmanmail@gmail.com>
2019-07-15 17:23:27 +0300

    feat: data version is same for saving many

--- src/data-version/data-version-middleware.ts
+++ src/data-version/data-version-middleware.ts
@@ -1,18 +1,33 @@
-import { Schema } from 'mongoose';
+import { Document, HookNextFunction, Schema } from 'mongoose';
 import { InnerModelType } from '../types';
 import { getNextDataVersion } from './data-version-manager';
 
 export const addDataVersionMiddleware = (schema: Schema) => {
-    ['findOneAndUpdate', 'update', 'updateOne', 'updateMany', 'save', 'insertMany'].forEach(
-        middleware => {
-            schema.pre(middleware, getDataVersionHandler());
-        },
-    );
+    ['findOneAndUpdate', 'update', 'updateOne', 'save'].forEach(middleware => {
+        schema.post(middleware, getDataVersionHandler());
+    });
+
+    ['updateMany', 'insertMany'].forEach(middleware => {
+        schema.post(middleware, getDataVersionManyHandler());
+    });
 };
 
 const getDataVersionHandler = () => {
-    return async function dataVersionSetter(this: InnerModelType<any>, next: () => void) {
-        this.dataVersion = await getNextDataVersion();
+    return async function dataVersionSetter(doc: InnerModelType<any>, next: () => void) {
+        doc.dataVersion = await getNextDataVersion();
         next();
+    };
+};
+
+const getDataVersionManyHandler = () => {
+    return async function manyDataVersionSetter(docs: InnerModelType<any>, next: () => void) {
+        const dataVersion = await getNextDataVersion();
+        docs.forEach(setDataVersion);
+
+        function setDataVersion(doc: any) {
+            doc.dataVersion = dataVersion;
+        }
+
+        return next();
     };
 };
